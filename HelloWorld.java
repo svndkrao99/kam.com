d97af1c9409e689ad30b891bf86d6de188ca9842
svndk99 <kamesh9.salesforce@gmail.com>
2023-05-08 21:22:02 +0530

    commited again

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-     System.out.println("Congratulations! Our First Jenkins Pipeline");
+     System.out.println("Congratulations! Our First Jenkins Freestyle Project");
   }
 }
